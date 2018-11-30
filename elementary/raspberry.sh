e952d2a83cc8c417432742495b05925e016a897f
Justin Swanepoel <Justin.Swanepoel@live.com>
2018-11-30 15:08:53 +0200

    Create raspberry.sh

---
+++ elementary/raspberry.sh
@@ -0,0 +1,36 @@
+hostname -I
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install some required packages first
+sudo apt update
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Get the Docker signing key for packages
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+# Add the Docker official repos
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# Install Docker
+sudo apt update
+sudo apt install docker-ce
+
+# Enable Docker at boot
+sudo systemctl enable docker
+sudo systemctl start docker
+
+
+sudo apt update
+sudo apt install -y python python-pip
+
+# Install Docker Compose from pip
+pip install docker-compose
